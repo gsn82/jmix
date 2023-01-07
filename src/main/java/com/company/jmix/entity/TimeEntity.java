590a0df39e9d0df0122e572bfdaaf2e212cbc421
gsn <gsn82@mail.ru>
2023-01-07 15:13:12 +0500

    TimeEntity

---
+++ src/main/java/com/company/jmix/entity/TimeEntity.java
@@ -0,0 +1,90 @@
+package com.company.jmix.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "TIME_ENTITY")
+@Entity
+public class TimeEntity {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @JoinColumn(name = "TASK_ID", nullable = false)
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private ProjectTask task;
+
+    @Column(name = "TIME_SPENT")
+    private Integer timeSpent;
+
+    @Column(name = "ENTRY_DATE", nullable = false)
+    @NotNull
+    private LocalDateTime entryDate;
+
+    @JoinColumn(name = "USER_ID", nullable = false)
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private User user;
+
+    @InstanceName
+    @Column(name = "DECRIPTION")
+    @Lob
+    private String decription;
+
+    public Integer getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(Integer timeSpent) {
+        this.timeSpent = timeSpent;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(LocalDateTime entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public ProjectTask getTask() {
+        return task;
+    }
+
+    public void setTask(ProjectTask task) {
+        this.task = task;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
